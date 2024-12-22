23c1cada6b6f598c2ef68d532e5c825e7a3b75ca
Bogdan Ceausu <Bogdan.Ceausu@protonmail.com>
2024-12-22 13:24:01 +0200

    debug controls

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+
+    -- stylua: ignore
+    keys = {
+      { "<F8>", function() require("dap").continue() end, desc = "Run/Continue" },
+      { "<F5>", function() require("dap").step_into() end, desc = "Step Into" },
+      { "<F7>", function() require("dap").step_out() end, desc = "Step Out" },
+      { "<F6>", function() require("dap").step_over() end, desc = "Step Over" },
+
+      -- { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+      -- { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+      -- { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
+      -- { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+      -- { "<leader>dg", function() require("dap").goto_() end, desc = "Go to Line (No Execute)" },
+      -- { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+      -- { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+      -- { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+      -- { "<leader>dP", function() require("dap").pause() end, desc = "Pause" },
+      -- { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+      -- { "<leader>ds", function() require("dap").session() end, desc = "Session" },
+      -- { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+      -- { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+    },
+  },
+}

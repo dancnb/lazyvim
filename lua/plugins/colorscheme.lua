22e6c83f9ab9d7d9a08fefd6caf9a9951362f993
Bogdan Ceausu <Bogdan.Ceausu@protonmail.com>
2025-01-03 18:58:07 +0200

    add colorizer plugin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,12 @@
 return {
+
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup()
+    end,
+  },
+
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
